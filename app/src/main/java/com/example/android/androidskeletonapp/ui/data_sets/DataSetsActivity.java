0f835536913edad2a94d19f60b8cac4e1897fea7
tendai14 <a.mutize.am@gmail.com>
2019-07-04 16:10:40 +0200

    data sets

--- app/src/main/java/com/example/android/androidskeletonapp/ui/data_sets/DataSetsActivity.java
+++ app/src/main/java/com/example/android/androidskeletonapp/ui/data_sets/DataSetsActivity.java
@@ -1,9 +1,18 @@
 package com.example.android.androidskeletonapp.ui.data_sets;
 
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.lifecycle.LiveData;
+import androidx.paging.PagedList;
 
 import com.example.android.androidskeletonapp.R;
+import com.example.android.androidskeletonapp.data.Sdk;
 import com.example.android.androidskeletonapp.ui.base.ListActivity;
+import com.example.android.androidskeletonapp.ui.d2_errors.D2ErrorAdapter;
+
+import org.hisp.dhis.android.core.dataset.DataSet;
+import org.hisp.dhis.android.core.maintenance.D2Error;
 
 public class DataSetsActivity extends ListActivity {
 
@@ -12,5 +21,23 @@ public class DataSetsActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setUp(R.layout.activity_data_sets, R.id.dataSetsToolbar, R.id.dataSetsRecyclerView);
         // TODO List DataSets
+
+        observeDataSets();
+
+    }
+
+    private void observeDataSets(){
+
+        DataSetsAdapter adapter = new DataSetsAdapter();
+        recyclerView.setAdapter(adapter);
+
+        LiveData<PagedList<DataSet>> liveData = Sdk.d2().dataSetModule().dataSets
+                .getPaged(15);
+
+        liveData.observe(this, dataSets -> {
+            adapter.submitList(dataSets);
+            findViewById(R.id.dataSetsNotificator).setVisibility(
+                    dataSets.isEmpty() ? View.VISIBLE : View.GONE);
+        });
     }
 }
